e0621e15f8be35759e5d6e7e2329ef23777b91dd
anujhanmante <56170753+anujhanmante@users.noreply.github.com>
2026-09-07 14:47:20 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.09
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "ksk"
+print(type(variable))
+variable = [12,212,211]
+print(type(variable))
